727990345fd6453c4cbfb7ae22ac76f83398ac9d
shaxbozjon-dev <shaxbozjon215@gmail.com>
2024-01-03 10:42:46 +0500

    horizontal tomonga utkazadi ekranni

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'package:starts_game/game_core/main_loop.dart';
+import 'package:flutter/cupertino.dart';
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,DeviceOrientation.landscapeRight   //ekranni(horizontal) yon tarafga buradi
+  ]).whenComplete(() {
+    SystemChrome.setEnabledSystemUIOverlays([
+      SystemUiOverlay.bottom
+    ])
+    runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(
+        child: Text("Game"),
+      ),
+    ));
+  });
 
-    ),
-  ));
 }
-
-
